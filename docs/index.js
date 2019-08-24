5887e68935e7a4190aa58d9126cea4f92e4310dc
Your Name <you@example.com>
2019-08-24 17:48:25 +0900

    サーバー処理実装、デバッグ用ページ追加。

--- docs/index.js
+++ docs/index.js
@@ -1,10 +1,17 @@
-const server = window.prompt("接続先を入力してください", "localhost");
+const server = window.prompt("接続先を入力してください", "localhost")
 
 const socket  = io(server);
 
+const form = document.forms.sendData;
+
 function onButtonClicked() {
   console.log("Clicked.");
-  socket.emit("send", "send");
+  const sendData = {
+    humidity: form.humidity.value,
+    latitude: form.latitude.value,
+    longitude: form.longitude.value
+  }
+  socket.emit("send", sendData);
 }
 
 console.log("test");
--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const http = require("http").createServer(app);
 const socketio = require("socket.io");
 const io = socketio(http);
+const geolib = require("geolib");
 
-app.get("/", function (req, res) {
+
+/* app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
   console.log("Request for index.html was received.");
 });
@@ -22,23 +24,38 @@ app.get("/output.html", function (req, res) {
 app.get("/output.js", function (req, res) {
   res.sendFile(__dirname + "/output.js");
   console.log("Request for output.js was received.");
-})
+}) */
+
+/*緯度: 35.689775 経度: 139.78256*/
+
+const mistLocation = {
+  latitude: 35.689775,
+  longitude: 139.78256
+}
+
+const storedData = [];
 
 io.on("connection", function (socket) {
   console.log("a user connected");
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
 
-  socket.on("location", function (location) {
-    console.log("Received a location data.");
-    console.log(location);
+  socket.on("send", function (receivedData) {
+    const senderLocation = {latitude: receivedData.latitude, longitude: receivedData.longitude};
+    const distance = geolib.getDistance(mistLocation, senderLocation, 0.1);
+    const nowDate = new Date;
+    const data = {humidity: receivedData.humidity, latitude: receivedData.latitude, longitude: receivedData.longitude, distance: distance, date: nowDate};
+    storedData.push(data);
+
+    const searchedData = storedData.filter(function(queryData) {
+      queryData.distance < 10 && nowDate.now() - queryData.date.now < 60000;
+    });
+    
+    if(searchedData.length < 10) {
+      io.emit("move", "MOVE");
+    }
   })
 
-  socket.on("send", function (message) {
-    console.log(message);
-    io.emit("move", "MOVE");
+  socket.on("test", function (data) {
+    console.log(data);
   })
 
   socket.on("disconnect", function () {
